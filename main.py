fe5d543b2f6f05501a7737da24972921eed7553d
ale.chaito <alechaito@gmail.com>
2021-08-19 14:17:13 -0300

    removed all functions from main

--- main.py
+++ main.py
@@ -1,70 +1,19 @@
-from static import *
+from static import motoboys
+from helpers import send_orders, check_motoboys_orders, get_motoboy_by_id
 
-import random
-import argparse
-import sys
-
-# Created using python3.7
 
 def main():
-    global motoboys, stores, orders
-    
     send_orders()
     while True:
-        motoboy_id = input("Send motoboy_id or 0 to get all or ctrl+c to exit:")
+        motoboy_id = input(
+            "Send motoboy_id or 0 to get all or ctrl+c to exit:")
         if(int(motoboy_id) == 0):
-            check_motoboys_orders()
+            check_motoboys_orders(motoboys)
         else:
-            motoboy = select_motoboy_by_id(int(motoboy_id)) 
-            if(motoboy != None): 
-                check_motoboy_orders(motoboy)
+            motoboy = get_motoboy_by_id(motoboy_id)
+            if(motoboy is not None):
+                check_motoboys_orders([motoboy])
 
 
-def select_motoboy_by_id(id):
-    global motoboys
-
-    for motoboy in motoboys:
-        if(motoboy.id == id):
-            return motoboy
-
-    print("Motoboy with id: {} not exist...".format(id))
-    return None
-
-def send_orders():
-    global orders
-    print("Sending orders ... \n")
-    for order in orders:
-        motoboy_selected = order.check_exclusivity(motoboys)
-        if(motoboy_selected == None):
-            motoboy_selected = random.choice(motoboys)
-        motoboy_selected.add_order(order)
-
-def check_motoboys_orders():
-    for index, motoboy in enumerate(motoboys):
-        print("Motoboy id : {}, Name: {}".format(motoboy.id, motoboy.name))
-        if(len(motoboy.orders_received) == 0):
-            print("Not received any order...")
-        for order in motoboy.orders_received:
-            print("Received order from store: {}, Value; R${}, Comission: R${}".format(
-                order.store.id,
-                order.value,
-                motoboy.win_by_order(order)
-            ))
-        print("Total winned: R${}".format(motoboy.total_win))
-        print("---------------------------- \n")
-
-def check_motoboy_orders(motoboy):
-    print("Motoboy id : {}, Name: {}".format(motoboy.id, motoboy.name))
-    if(len(motoboy.orders_received) == 0):
-        print("Not received any order...")
-    for order in motoboy.orders_received:
-        print("Received order from store: {}, Value; R${}, Comission: R${}".format(
-            order.store.id,
-            order.value,
-            motoboy.win_by_order(order)
-        ))
-    print("Total winned: R${}".format(motoboy.total_win))
-    print("---------------------------- \n")
-
-
-main()
+if __name__ == "__main__":
+    main()
