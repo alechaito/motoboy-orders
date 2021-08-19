f8b82d985959a33442ba08af09bd234ae6ced39a
ale.chaito <alechaito@gmail.com>
2021-08-19 14:17:34 -0300

    remove old comments and add repr print

--- motoboy.py
+++ motoboy.py
@@ -1,29 +1,27 @@
 import names
 
+
 class Motoboy:
 
-    def __init__(self, id, charge_value, stores, exclusivity = None):
+    def __init__(self, id, charge_value, exclusivity=None):
         self.id = id
         self.charge_value = charge_value
-        self.stores = stores # array from 0 to 2
         self.name = names.get_full_name()
         self.total_win = 0
         self.total_orders = 0
         self.exclusivity = exclusivity
         self.orders_received = []
-    
+
+    def __repr__(self):
+        return f"""Motoboy id: {self.id}, \
+        Name: {self.name}, \
+        TotalWin: {self.total_win} \
+        """
+
     def win_by_order(self, order):
-        return order.value * order.store.comission +  self.charge_value
-    
+        return order.value * order.store.comission + self.charge_value
 
     def add_order(self, order):
         self.total_orders += 1
         self.total_win += self.win_by_order(order)
         self.orders_received.append(order)
-        """print("Motoboy {} received new order / Winned: {} / Total_win: {} / Total orders: {}".format(
-            self.name,
-            self.win_by_order(order),
-            self.total_win,
-            self.total_orders
-        ))
-        print("-------------------------- \n")"""
